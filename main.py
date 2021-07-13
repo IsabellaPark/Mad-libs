8b048d7cf4fcc33c3e5b3d8e4b0eedfd062b9f90
ParkBella <87280152+IsabellaPark@users.noreply.github.com>
2021-07-13 21:03:09 +0000

    I coded a madlib

---
+++ main.py
@@ -0,0 +1,13 @@
+name = input("What is your name? ")
+print("hi there " + name)
+
+
+verb1 = input("Enter a verb: ")
+adj1 = input("Enter a adjective: ")
+noun1 = input("Enter a noun: ")
+verb2 = input("Enter a second verb: ")
+noun2 = input("Enter a second noun: ")
+adj2 = input("Enter a second adjective: ")
+print("Loading story---------------------------")
+print("Here is the story: ")
+print("It was a " + adj1 + " day outside. " + name + " was very bored, so they decided to go on a " + verb1 + ", to " + noun1 + ". " + name + " bumped into something " + adj1 + " and screamed. They quickly went " + verb2 + " back to " + noun2 + " where they ate the " + adj2 + "food." )
